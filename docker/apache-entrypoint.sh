e88cab5f83b2ad59ef613c6bd2b87f77cbfb695f
Vojtěch Voleman <vojtech.voleman@tul.cz>
2023-12-18 19:40:35 +0100

    Fix error

--- docker/apache-entrypoint.sh
+++ docker/apache-entrypoint.sh
@@ -2,7 +2,7 @@ docker/wait-for-it.sh mysql:3306 -t 30
 
 # Run migrations
 composer update --no-interaction --no-progress --no-suggest
-#php bin/console doctrine:migrations:migrate --no-interaction && \
+php bin/console doctrine:migrations:migrate --no-interaction
 #php bin/console syncer:all:run --no-interaction --no-debug
 
 echo "Setup for API is done!"
